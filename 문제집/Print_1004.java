7e9cba5d1dc12d6c4aa6c8953248be8a18f95863
cm4ng <dudrbs703@naver.com>
2020-09-22 15:44:51 +0900

    해결한 문제 추가
    1004, 1005

---
+++ 문제집/Print_1004.java
@@ -0,0 +1,9 @@
+/**
+ * Print_1004
+ */
+public class Print_1004 {
+
+    public static void main(String[] args) {
+        System.out.println("'Hello'");
+    }
+}
---
+++ 문제집/Print_1005.java
@@ -0,0 +1,9 @@
+/**
+ * Print_1005
+ */
+public class Print_1005 {
+
+    public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }
+}

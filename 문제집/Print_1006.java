d565a0581626a8858623ac40f983248ddad90f55
cm4ng <dudrbs703@naver.com>
2020-09-23 09:20:08 +0900

    해결한 문제 추가
    1006, 1007

---
+++ 문제집/Print_1006.java
@@ -0,0 +1,10 @@
+/**
+ * Print_1006
+ */
+public class Print_1006 {
+
+    public static void main(String[] args) {
+        System.out.println("\"!@#$%^&*()\"");
+        
+    }
+}
---
+++ 문제집/Print_1007.java
@@ -0,0 +1,9 @@
+/**
+ * Print_1007
+ */
+public class Print_1007 {
+
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}

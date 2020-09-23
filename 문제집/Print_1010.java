e70f67645f91cf28eb1519efe103fb4d59e829ae
cm4ng <dudrbs703@naver.com>
2020-09-24 00:24:17 +0900

    해결한 문제 추가
    1008, 1010번

---
+++ 문제집/Print_1010.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Print_1010
+ */
+public class Print_1010 
+{
+    public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.close();
+        System.out.println(input); 
+    }
+}

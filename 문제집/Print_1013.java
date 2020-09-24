0124682a4f9fd12143d394d1de57af770c0cdea7
cm4ng <dudrbs703@naver.com>
2020-09-25 08:46:28 +0900

    해결한 문제 추가
    1013, 1014

---
+++ 문제집/Print_1013.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Print_1012
+ */
+public class Print_1013
+{
+    public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        scanner.close();
+
+        System.out.printf(a+" "+b);
+    }
+}
---
+++ 문제집/Print_1014.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Print_1012
+ */
+public class Print_1014
+{
+    public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String b = scanner.next();
+        scanner.close();
+
+        System.out.printf(b+" "+a);
+    }
+}

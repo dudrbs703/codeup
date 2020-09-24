cddffd88960674b8ca6289e6ea75e37e0d3e2f45
cm4ng <dudrbs703@naver.com>
2020-09-25 08:46:49 +0900

    해결한 문제 추가
    1015, 1017

---
+++ 문제집/Print_1015.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Print_1012
+ */
+public class Print_1015
+{
+    public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextFloat();
+        scanner.close();
+
+        System.out.printf(String.format("%.2f", a));
+    }
+}
---
+++ 문제집/Print_1017.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Print_1012
+ */
+public class Print_1017
+{
+    public static void main(String[] args)
+     {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        scanner.close();
+
+        for(int i=0; i<3; i++)
+        {
+            System.out.println(x);
+        }
+    }
+}

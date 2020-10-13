5515e8cda8fd0a9a74f217f90621eb7a262c8757
cm4ng <dudrbs703@naver.com>
2020-10-13 16:26:02 +0900

    해결한 문제 추가
    1050, 1051, 1052, 1053, 1054, 1055

---
+++ 문제집/Print_1053.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Print_1053
+ */
+public class Print_1053 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        scanner.close();
+
+        if(a == 0) System.out.println("1");
+        else System.out.println("0");
+    }
+}

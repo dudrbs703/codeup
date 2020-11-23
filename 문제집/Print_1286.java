8069caffd26321a5938035bd44146383006af1e9
cm4ng <dudrbs703@naver.com>
2020-11-23 18:49:05 +0900

    해결한 문제 추가
    1286, 1451, 1452, 1709

--- 문제집/Print_1286.java
+++ 문제집/Print_1286.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -7,7 +9,30 @@ import java.util.Scanner;
 public class Print_1286 {
 
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-       
+        
+        Scanner scanner = new Scanner(System.in);
+        int array[] = new int[5];
+        for(int i=0; i<array.length; i++)
+        {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        int size = array.length;
+        for(int i=1; i<=array.length;i++)
+        {
+            for(int j=0; j<array.length-i; j++)
+            {
+                if(array[j] >= array[j+1])
+                {
+                    int a = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = a;
+                }
+            }
+        }
+
+        System.out.println(array[size-1]);
+        System.out.println(array[0]);
     }
 }
---
+++ 문제집/Print_1452.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * Print_1452
+ */
+public class Print_1452 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=0; i<a; i++)
+        {
+            queue.offer(scanner.nextInt());
+        }
+        scanner.close();
+
+        while(!queue.isEmpty()) System.out.println(queue.poll());
+    }
+}

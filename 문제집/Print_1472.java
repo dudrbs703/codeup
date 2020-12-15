a82bacb1493e662b052185b937f2c70b607491e7
cm4ng <dudrbs703@naver.com>
2020-12-15 14:24:11 +0900

    해결한 문제 추가
    1472, 1473

---
+++ 문제집/Print_1472.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+/**
+ * Print_1472
+ */
+public class Print_1472 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        scanner.close();
+
+        int [][]data = new int[a][b];
+        
+        int level = data.length-1;
+        int count = 1;
+
+        boolean isReversed = true;
+        
+        while(level >= 0)
+        {
+            if(!isReversed)
+            {
+                for(int i=0; i<data[level].length; i++)
+                {
+                    data[level][i] = count++;
+                }
+
+                isReversed = true;
+            } else {
+
+                for(int i=data[level].length-1; i>=0; i--)
+                {
+                    data[level][i] = count++;
+                }
+            
+                isReversed = false;
+               
+            }
+            level--;
+        }
+
+        for(int i=0;i<data.length;i++)
+        {
+            for(int j=0; j<data[i].length; j++)
+            {
+                System.out.print(data[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
